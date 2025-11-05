49c803c6aa1fcafacab457632e73e9c3ba47fadb
Jason Odoom <jasonodoom@riseup.net>
2025-11-04 19:44:00 -0500

    Revert "Add temporary root user for debugging"
    This reverts commit 2c03b88e08c9d4501146293ad161e508a004cd1b.

--- framework-desktop/modules/user-config.nix
+++ framework-desktop/modules/user-config.nix
@@ -2,11 +2,6 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
-  # Temporary root user for debugging
-  users.users.root = {
-    initialPassword = "nixos-debug";
-  };
-
   # User account
   users.users.jason = {
     isNormalUser = true;
