178367ba4577834b6cff20a06aeea3a402a5f091
Jason Odoom <jasonodoom@riseup.net>
2025-11-04 22:30:07 -0500

    Fix SDDM astronaut theme installation

--- framework-desktop/modules/hyprland/hyprland.nix
+++ framework-desktop/modules/hyprland/hyprland.nix
@@ -348,6 +348,13 @@
     kdePackages.breeze
     kdePackages.breeze-icons
 
+    # SDDM Astronaut theme
+    (pkgs.fetchFromGitHub {
+      owner = "Keyitdev";
+      repo = "sddm-astronaut-theme";
+      rev = "468a100460d5feaa701c2215c737b55789cba0fc";
+      sha256 = "sha256-L+5xoyjX3/nqjWtMRlHR/QfAXtnICyGzxesSZexZQMA=";
+    })
   ];
 
   # Enable required services
