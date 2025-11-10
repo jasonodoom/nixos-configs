a6cfd216338d68dda42662e975d535aa0ed59b89
Jason Odoom <jasonodoom@riseup.net>
2025-11-10 01:36:31 -0500

    Add agenix secrets management module

---
+++ framework-desktop/modules/secrets.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Import agenix secrets
+  age = {
+    secrets = {
+      # User password for login
+      jason-password = {
+        file = ../secrets/jason-password.age;
+        owner = "jason";
+        group = "users";
+        mode = "0400";
+      };
+    };
+
+    # Use the system's SSH host key for decryption
+    identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  };
+
+}
