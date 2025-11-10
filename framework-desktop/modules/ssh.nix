37b56383b42e6572e01d10639fa60b9e323007f5
Jason Odoom <jasonodoom@riseup.net>
2025-11-10 02:04:18 -0500

    Add SSH configuration and agenix secrets setup

--- framework-desktop/modules/ssh.nix
+++ framework-desktop/modules/ssh.nix
@@ -1,7 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-  # SSH server configuration
   services.openssh = {
     enable = true;
     ports = [ 666 ];
@@ -16,79 +15,53 @@
     extraConfig = "AllowUsers jason";
   };
 
-  # Enable SSH agent authentication
   security.pam.sshAgentAuth.enable = true;
   security.pam.services.sudo.sshAgentAuth = false;
 
-  # GPG agent handles SSH (YubiKey support)
   programs.ssh.startAgent = false;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
   };
+  environment.etc."ssh/ssh_config".text = ''
+    Host *
+      Protocol 2
+      HashKnownHosts yes
+      PasswordAuthentication no
+      StrictHostKeyChecking ask
+      PubkeyAuthentication yes
+      IdentitiesOnly yes
+      VisualHostKey yes
+      LogLevel INFO
+      ControlMaster auto
+      ControlPath ~/.ssh/controlmasters/%r@%h:%p
+      ControlPersist 10m
+      UseRoaming no
+      ServerAliveInterval 60
+      ServerAliveCountMax 3
+      KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512
+      HostKeyAlgorithms ssh-ed25519,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521,rsa-sha2-512,rsa-sha2-256
+      Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+      MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
+      IdentityFile ~/.ssh/id_ed25519
+      IdentityFile ~/.ssh/id_rsa
+      User jason
 
-  # SSH client configuration
-  programs.ssh = {
-    enable = true;
+    Host github.com
+      HostName github.com
+      User git
+      PubkeyAuthentication yes
+      IdentitiesOnly yes
+      IdentityFile ~/.ssh/id_rsa_yubikey.pub
 
-    # Global SSH client settings
-    extraConfig = ''
-      # Global defaults for all hosts
-      Host *
-        # Protocol and security settings
-        Protocol 2
-        HashKnownHosts yes
-        PasswordAuthentication no
-        StrictHostKeyChecking ask
-        PubkeyAuthentication yes
-        IdentitiesOnly yes
-        VisualHostKey yes
-        LogLevel INFO
+    Host gitlab.com
+      HostName gitlab.com
+      User git
+      PubkeyAuthentication yes
+      IdentitiesOnly yes
+      IdentityFile ~/.ssh/id_rsa_yubikey.pub
+  '';
 
-        # Connection multiplexing for performance
-        ControlMaster auto
-        ControlPath ~/.ssh/controlmasters/%r@%h:%p
-        ControlPersist 10m
-
-        # Security hardening
-        UseRoaming no
-        ServerAliveInterval 60
-        ServerAliveCountMax 3
-
-        # Modern cryptographic preferences
-        KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512
-        HostKeyAlgorithms ssh-ed25519,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521,rsa-sha2-512,rsa-sha2-256
-        Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
-        MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
-
-        # Default identity files (Ed25519 preferred, then RSA fallback)
-        IdentityFile ~/.ssh/id_ed25519
-        IdentityFile ~/.ssh/id_rsa
-
-        # Default user
-        User jason
-
-      # GitHub configuration with YubiKey
-      Host github.com
-        HostName github.com
-        User git
-        PubkeyAuthentication yes
-        IdentitiesOnly yes
-        IdentityFile ~/.ssh/id_rsa_yubikey.pub
-
-      # GitLab configuration (if needed)
-      Host gitlab.com
-        HostName gitlab.com
-        User git
-        PubkeyAuthentication yes
-        IdentitiesOnly yes
-        IdentityFile ~/.ssh/id_rsa_yubikey
-        IdentityFile ~/.ssh/id_ed25519_yubikey
-        IdentityAgent SSH_AUTH_SOCK
-    '';
-  };
-
-  # Ensure SSH control masters directory exists
   systemd.user.services.ssh-controlmasters = {
     description = "Create SSH ControlMaster directory";
     wantedBy = [ "default.target" ];
@@ -99,11 +72,7 @@
     };
   };
 
-  # SSH client packages
   environment.systemPackages = with pkgs; [
     openssh
   ];
-
-  # Ensure proper SSH agent is running (GPG agent handles SSH with YubiKey support)
-  # This is already configured in security.nix with programs.gnupg.agent.enableSSHSupport = true
 }
---
+++ framework-desktop/secrets/secrets.nix
@@ -0,0 +1,28 @@
+# Agenix secrets configuration file
+# This file defines which public keys can decrypt which secrets
+
+let
+  jason-scalene = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKQRbcTH0OZCQciQLgFXDqqqbc0383pXA/65JlZqpCyQ jason@scalene.local";
+  jason-theophany = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIICUc9Otz8oBlWJ1y5oc9x2dBnSJ4Zi3rzJnlAz+eEV7 jason@theophany.local";
+  jason-perdurabo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICwLk94aSzaUrpxHZ6BHbxMaF3054VZJh6rUF8cdSHIm jason@perdurabo";
+  jason-yk = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDdTRD5etaWB3UmGiJ2cD/TVCn/asEw7c8frhAYDOhsb1bmEp7z3mG7gKFwepBaWFX3D7aXXirTTNsnKd7AsM5riQQg1tZ5qtmT+nEmpDhi1WVtFm89jc0ezyJN1SnlsCUEhQ0twn4qzR+PnjRVE1E4KTpbwTCapgMl9w4iCEQikaPWWcg9u+CRGNLaehgM7Jm5jKdVoIa258wNgvCrNZcba4LCccz1PK5j4j1uu3sr400CatIEkWe+aqiDCBIamFPXuJqZy1gb4+dqk1wKPJqn8L9WFD6j5mDarrIaHHmy7rnviPinbpLoCE3eksxAVeI1QjI8uPXyrn4GtUQNSNBMZPu2DTCZSo5bG5NbcE2Di9KSkW8SQJg0dYgZSJjssp5qkT9uFx7AnLfvIlR3+IQA45cXnM+jXCikNbGPLMenv8jjMrSke73hxr8T6rsjO2FGT3tWeiDBN5B59wgWY+bbrExOcFe2/cClYfBFzdF9d800Xg6+fN7E6gamTyrNNRL68f+sawuTDBrWggPJFFcHvQMd4zxE/ujbyCgy+11U8M5AAU/y6/Aa2XUt0jnEXgMXBpo7M3/5OWRzzyCO2RwtDWVxrJXPW9xYGvSoPAfDmdi0VNiGyldvbw4HHcHiFqftTCrNzMbR/QbjsuF4HMGI4fXddWYOFlNHbv+X+O2/kQ== cardno:5252959";
+  hdd-backup = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGqpB62GkdvoDGnThDpLzCVsCZKiN8dNpxcwmpz08FrV jason@perdurabo-backup";
+
+
+  # System host key (will be generated during installation)
+  # /etc/ssh/ssh_host_ed25519_key.pub
+  perdurabo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFXyQoWYsAd796hWs6RRr9RMlTboe4KBxqk6mS+Ia8qJ";
+
+  # User keys that can decrypt secrets
+  users = [ jason-scalene jason-theophany jason-perdurabo jason-yk hdd-backup ];
+
+  # System keys (host keys)
+  systems = [ perdurabo ];
+
+  # All keys that can decrypt
+  all = users ++ systems;
+in
+{
+  # User password secret
+  "jason-password.age".publicKeys = all;
+}
