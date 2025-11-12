06a7ff747e7980b2e82c95f93deadcc3fc0881c5
Jason Odoom <jasonodoom@riseup.net>
2025-11-11 20:04:15 -0500

    Enable IPv6 forwarding and UDP GRO for Tailscale subnet routing

--- framework-desktop/modules/networking.nix
+++ framework-desktop/modules/networking.nix
@@ -27,7 +27,30 @@
       enable = true;
       allowPing = false;
       allowedTCPPorts = [ 6666 ]; # SSH port
+      checkReversePath = "loose";
     };
+  };
+
+  # Enable IP forwarding for Tailscale subnet routing
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
+
+  # Enable UDP GRO forwarding for Tailscale performance
+  # https://tailscale.com/s/ethtool-config-udp-gro
+  systemd.services.tailscale-udp-gro = {
+    description = "Enable UDP GRO forwarding for Tailscale";
+    after = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = ''
+      NETDEV=$(${pkgs.iproute2}/bin/ip -o route get 8.8.8.8 | cut -f 5 -d " ")
+      ${pkgs.ethtool}/bin/ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off || true
+    '';
   };
 
   # SSH configuration moved to security.nix to avoid duplication
