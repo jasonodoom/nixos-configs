8ad230860f15f8758d4fdb00129cda08fd7d1662
Jason Odoom <jasonodoom@riseup.net>
2025-11-11 19:51:27 -0500

    add arrow key navigation

--- framework-desktop/modules/bash/bash.nix
+++ framework-desktop/modules/bash/bash.nix
@@ -15,4 +15,24 @@
     completion.enable = true;
     enableLsColors = true;
   };
+
+  # Readline configuration for proper arrow key history navigation
+  environment.etc."inputrc".text = ''
+    # Arrow key history search
+    "\e[A": history-search-backward
+    "\e[B": history-search-forward
+    "\e[C": forward-char
+    "\e[D": backward-char
+
+    # Enable 8-bit input
+    set input-meta on
+    set output-meta on
+    set convert-meta off
+
+    # Completion settings
+    set show-all-if-ambiguous on
+    set completion-ignore-case on
+    set mark-symlinked-directories on
+    set visible-stats on
+  '';
 }
