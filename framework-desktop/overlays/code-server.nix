dd179620580fadfc8f79ac43cc58e83e0c476732
Jason Odoom <jasonodoom@riseup.net>
2025-11-15 20:27:27 -0500

    Update code-server overlay: Node.js 22, disable patches, fix yarnCache hash

--- framework-desktop/overlays/code-server.nix
+++ framework-desktop/overlays/code-server.nix
@@ -2,6 +2,7 @@
 let
   version = "4.105.1";
   hash = "sha256-75k2Vugv+46oVG/Ppxdn7uWryDR4gzj4uSVFNY6YAQM=";
+  commit = "811ec6c1d60add2eb92446161ca812828fdbaa7f";
 in
 final: prev: {
   code-server =
@@ -9,7 +10,8 @@ final: prev: {
       (prev.code-server.override {
         nodejs = final.nodejs_22;
       }).overrideAttrs (oldAttrs: {
-        inherit version;
+        inherit version commit;
+
         src = prev.fetchFromGitHub {
           owner = "coder";
           repo = "code-server";
@@ -17,6 +19,21 @@ final: prev: {
           fetchSubmodules = true;
           inherit hash;
         };
+
+        # Disable nixpkgs patches for older version
+        patches = [ ];
+
+        yarnCache = oldAttrs.yarnCache.overrideAttrs (oldCacheAttrs: {
+          inherit version;
+          src = prev.fetchFromGitHub {
+            owner = "coder";
+            repo = "code-server";
+            rev = "v${version}";
+            fetchSubmodules = true;
+            inherit hash;
+          };
+          outputHash = "sha256-3xDinhLSZJoz7N7Z/+ttDLh82fwyunOTeSE3ULOZcHA=";
+        });
       })
     else
       prev.writeShellScriptBin "code-server" ''
